56e679b3f6904d01c34c02248dd28b3afdb37cc9
Angelito Casagrande <angelito@angelito.com.br>
2022-08-14 00:25:49 -0300

    Add base IoC layer

---
+++ application/GoFinStrategy.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,37 @@
+﻿using GoFinStrategy.Application.Interfaces.Categories;
+using GoFinStrategy.Application.UseCases.Categories;
+using GoFinStrategy.CrossCutting.Shared.Contexts;
+using GoFinStrategy.CrossCutting.Shared.Interfaces;
+using GoFinStrategy.Domain.Interfaces;
+using GoFinStrategy.Domain.Interfaces.Repositories;
+using GoFinStrategy.Infrastructure.Data.Postgresql.Context;
+using GoFinStrategy.Infrastructure.Data.Postgresql.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoFinStrategy.IoC
+{
+    public static class NativeInjectorBootStrapper
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            // Shared Context
+            services.AddScoped<INotificationContext, NotificationContext>();
+
+            // Database
+            services.AddDbContext<DataContext>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            // Use Cases
+            services.AddTransient<IAddCategoryUseCase, AddCategoryUseCase>();
+            services.AddTransient<IDeleteCategoryByIdUseCase, DeleteCategoryByIdUseCase>();
+            services.AddTransient<IGetAllCategoriesUseCase, GetAllCategoriesUseCase>();
+            services.AddTransient<IGetCategoryByIdUseCase, GetCategoryByIdUseCase>();
+            services.AddTransient<IUpdateCategoryUseCase, UpdateCategoryUseCase>();
+
+            // Repositories
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+        }
+    }
+}
